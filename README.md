04e0feaef9f09037978dcaeb4819d7c3f8e34acc
ShorenaK <shorenaanzhilov@gmail.com>
2024-06-22 10:43:20 -0400

    updated image files

---
+++ README.md
@@ -0,0 +1,30 @@
+Shorena K. Anzhilov SEI-Preject 1 - BlackJack
+
+Game Descriptions:
+
+This is the card game called "BlackJack", played at the table of 2-7 players against the dealer. The main goal of the game is to get to the value of 21 which is the "BlackJack". In my game only the dealer and one player play against eachother,  game starts by clicking the "Kick off the game" button, two cards per each are shuflled, the aim is to beat the dealer by getting a score as close as possible to 21 or even to 21 it's self. After  card are dealt player has an option to draw a new card, I have created the new card button so that player can generate another set of card, if the  "New Card" button is clicked and sum of the cards is over 21 one, player will have a message displayed accordingly "Bust! You lost a Bet!", if player gets number 21 a message  will appear "Player Wins Black Jack!!!", in addition player gets 50 chips added to the total of chips that player has. Player starts with 1000 chips and if he loses, 50 chips will be deducted from the total and in case of winnings 50 chips will be added. The value of the cards: 2 - 10 worth their face value. Jack, Queen and King are also worth of 10, and ace is defaulted to 11 or it can be change to one. In my game I have slightly different set up since I only have numbers displayed, I have implemented the function that generates numbers from 1 to 11, if player and dealer get the same value  of cards, a message will appear stating that " it's TIE", in case of dealer hitting the black Jack, player automatically loses, plus 50 chips will be deducted and message "'Table Wins Black Jack!!!" will be displyed. This game also has the third button to navigate to the main page which is the entrence of the casion, that is the starting point of entering Shorena's Black Jack Table!
+
+
+
+The Tech stack:
+
+I used HTML, CSS, and JavaScript. I have two files of HTML, the main page and the page for the game. I also have two CSS files linked accordingly. I have one JavaScript file for all of the functionality. 
+
+
+Challenging Code Parts:
+
+Challenging  part was creating the render function which I define as a heart of the game. 
+
+
+Support:
+For any questions regarding this project, please email me directly at shorenaanzhilov@gmail.com
+
+
+
+Link to Game:
+
+https://shorenak.github.io/SEI-BlackJack/
+
+
+Roadmap:
+I do not anticipate to change anything on this project, I would like leave it as a reminder of my first step of this meaningful and remarkable journey. 
---
+++ blackjack.js
@@ -0,0 +1,93 @@
+let chips = 50
+let sum = 0 // updated in the kikkOff function
+let sumDlCards = 0
+let hasBlackJack = false// game has not started no blackjack
+let isInGame = false // game has not started so palyer is not in game
+let messageDl = '' // dealer's Balck Jack message
+let message = '' // empty string will be updated in the functione
+let cardDeck = [] // empty array for the player's card
+let cardDeckDl = [] // empty array for the Delealr's card
+let player = {
+  name: 'Player',
+  chips: 1000
+}
+let displaymessage = document.querySelector('#message')
+let displaymessageDl = document.querySelector('#message-dealer')
+let totalTag = document.getElementById('total')
+let totalDl = document.getElementById('dealer_total')
+let cardsTag = document.querySelector('#player-card')
+let dlCardsTag = document.querySelector('#dealer_card')
+let playerTag = document.querySelector('#player_chips')
+// shuffling the deck of cards from 1 - 13
+function shuffleNewCard(){
+  let randomNum = Math.floor(Math.random() * 13) + 1
+  if (randomNum > 10){
+  return 10 // kings / Jack / Queen
+  } else if (randomNum === 1){
+  return 11 // Ace
+  }else{
+  return randomNum // rest  of deck of cards
+  }
+}
+function kickOffGame() {
+  // player is inGame / true/ and add the generating the cards/ sum
+  isInGame = true
+  let cardOne = shuffleNewCard() // player's cards
+  let cardTwo = shuffleNewCard()
+  cardDeck = [cardOne, cardTwo]
+  sum = cardOne + cardTwo
+  let cardOneDl = shuffleNewCard() // dealer's card
+  let cardTwodDl = shuffleNewCard()
+  cardDeckDl = [cardOneDl, cardTwodDl]
+  sumDlCards = cardOneDl + cardTwodDl
+
+  renderGame()
+}
+// function to dipslay the messages and renders the game / heart of the game
+function renderGame (){
+  dlCardsTag.textContent = `Dealer's Cards:`
+  cardsTag.textContent = `Player's Cards: `
+for (i = 0; i < cardDeck.length; i ++){
+      cardsTag.textContent = `${cardsTag.textContent}  ${cardDeck[i]}`
+  }
+for (i = 0; i < cardDeckDl.length; i ++){
+  dlCardsTag.textContent =  `${dlCardsTag.textContent}  ${cardDeckDl[i]}`
+  }
+  totalDl.textContent = `Total: ${sumDlCards}`
+  totalTag.textContent = `Total: ${sum}`
+  message = ''
+  messageDl = ''
+if(sumDlCards === 21){
+  messageDl = 'Table Wins Black Jack!!!'
+  message = 'Player lost!'
+  player.chips = player.chips - chips
+  isInGame = false
+  } else if(sum === 21){
+  message = 'Player wins Black Jack!!!'
+  player.chips += chips
+  hasBlackJack = true
+  }else if(sum === sumDlCards){
+  messageDl = `It's TIE`
+  message = `It's TIE`
+  }else if(sum <= 20){
+  isInGame = true
+  message = 'Would you like to hit?'
+  }else if (sum > 21) {
+  message = " Bust! You lost a Bet!"
+  player.chips = player.chips - chips
+  isInGame = false
+}
+ displaymessageDl.textContent = messageDl
+ displaymessage.textContent = message
+ playerTag.textContent = `${player.name} has ${player.chips} chips`
+}
+function newCard(){
+  // if the palyer has lost cannot picked the new card
+  // while player isInGame is able to pick the card
+if (isInGame === true && hasBlackJack !== true){
+  let card = shuffleNewCard()
+  sum += card
+  cardDeck.push(card)
+  renderGame()
+ }
+}
